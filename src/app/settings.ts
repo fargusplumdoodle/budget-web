b27d9f8674c089f37756b1d72a3baba79a396f1a
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-28 07:42:32 -0800

    refactor(router): Auth flow will work in production

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,8 +1,7 @@
-// TODO: FIX ALL OF THIS
+import { getServerURL } from "../util/window";
+
 const CLIENT_ID = "web-client";
-const CALLBACK_URL = `${window.location.protocol}//${window.location.hostname}${
-  window.location.port === "3000" ? ":" + window.location.port : ""
-}/auth/callback`;
+const CALLBACK_URL = `${getServerURL()}/auth/callback`;
 
 const settings = {
   auth: {
@@ -12,7 +11,7 @@ const settings = {
       "8obfUZIyyTcwHjCcl2Fv51P1OilFsgvp8TxHsBXyTawDi0Lozr2kAIhy6Z4bjJchP32SqUzezv1N0BxO0cZ02JMK2jsNZMG6jCd8sp9ejYDvUqVW2XePshD3COCPlv",
     callbackUrl: CALLBACK_URL,
     ApiLoginUrl:
-      "http://127.0.0.1:8000/o/authorize" +
+      `${getServerURL(true)}/o/authorize` +
       "?response_type=code" +
       `&client_id=${CLIENT_ID}` +
       `&redirect_uri=${CALLBACK_URL}`,
---
+++ src/util/window.ts
@@ -0,0 +1,7 @@
+export function getServerURL(api = false): string {
+  const webPort =
+    window.location.port === "3000" ? ":" + window.location.port : "";
+  const port =
+    api && process.env.NODE_ENV === "development" ? ":8000" : webPort;
+  return `${window.location.protocol}//${window.location.hostname}${port}`;
+}
