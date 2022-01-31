0338166ddc51623e3128a3b97587ba0386a8cf60
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-31 07:58:17 -0800

    refactor(forms): Added tabs to transaction page

--- src/pages/transactions_add/AddTransactionsPage.tsx
+++ src/pages/transactions_add/AddTransactionsPage.tsx
@@ -1,13 +1,25 @@
-import { Box, Card } from "@mui/material";
+import { Box, Card, Tab, Tabs } from "@mui/material";
 import * as React from "react";
-import "./AddTransactionsPage.css";
 import { FunctionComponent, useState } from "react";
+import "./AddTransactionsPage.css";
 import TransactionForm from "../../components/common/forms/transaction/TransactionForm";
 import TransactionTable from "../../components/transactions/transactions_table/TransactionsTable";
 import { Transaction } from "../../store/types/models";
+import IncomeForm from "../../components/common/forms/transaction/IncomeForm/IncomeForm";
+import { TabPanel, tabProps } from "../../components/common/tabs";
+
+const ADD = 0;
+const INCOME = 1;
+const TRANSFER = 2;
 
 const AddTransactionsPage: FunctionComponent = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [tab, setTab] = useState<number>(ADD);
+
+  const handleTabChange = (_: React.SyntheticEvent, newTab: number) => {
+    setTab(newTab);
+  };
+
   return (
     <>
       <h2>Add Transactions</h2>
@@ -17,13 +29,40 @@ const AddTransactionsPage: FunctionComponent = () => {
           justifyContent: "space-between",
         }}
       >
-        <Card sx={{ width: "38%", m: 1, p: 1, maxHeight: 540 }}>
-          <p className="transactionFormHeader">Transaction</p>
-          <TransactionForm
-            onCreateCallback={(trans) => {
-              setTransactions([trans, ...transactions]);
+        <Card sx={{ width: "38%", m: 1, p: 1 }}>
+          <Tabs
+            value={tab}
+            onChange={handleTabChange}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
             }}
-          />
+          >
+            <Tab label="Add" {...tabProps(ADD)} />
+            <Tab label="Income" {...tabProps(INCOME)} />
+            <Tab label="Transfer" {...tabProps(TRANSFER)} />
+          </Tabs>
+
+          <TabPanel value={tab} index={ADD}>
+            <TransactionForm
+              onCreateCallback={(trans) => {
+                setTransactions([trans, ...transactions]);
+              }}
+            />
+          </TabPanel>
+
+          <TabPanel value={tab} index={INCOME}>
+            <IncomeForm
+              onCreateTransactions={(newTransactions: Transaction[]) => {
+                setTransactions([...newTransactions, ...transactions]);
+              }}
+            />
+          </TabPanel>
+
+          <TabPanel value={tab} index={TRANSFER}>
+            <h1>TODO: TRANSFER FORM</h1>
+          </TabPanel>
         </Card>
 
         <Card sx={{ width: "62%", m: 1 }}>
