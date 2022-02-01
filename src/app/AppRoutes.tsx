c3087a8098c98d0ba81d8e2bdaf553fb5a57211e
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-31 18:01:00 -0800

    refactor(routes): Removed add income page

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -5,7 +5,6 @@ import Callback from "../components/auth/Callback";
 import DashboardPage from "../components/dashboard/DashboardPage";
 import TransactionsPage from "../pages/transactions_list/TransactionsPage";
 import AddTransactionsPage from "../pages/transactions_add/AddTransactionsPage";
-import AddIncomePage from "../pages/transactions_income/AddIncomePage";
 
 const AppRoutes: FunctionComponent<{}> = () => {
   return (
@@ -14,7 +13,6 @@ const AppRoutes: FunctionComponent<{}> = () => {
       <Route path="/auth/callback" element={<Callback />} />
       <Route path="/transactions" element={<TransactionsPage />} />
       <Route path="/transactions/add" element={<AddTransactionsPage />} />
-      <Route path="/transactions/income" element={<AddIncomePage />} />
     </Routes>
   );
 };
