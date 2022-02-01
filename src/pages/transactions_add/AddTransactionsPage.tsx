cdb14c4bfe5bd1fac3a16842c4d32d2fae8fe0d8
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-31 19:11:21 -0800

    reactor(forms): Moved IncomeFormData type to IncomeForm.tsx

--- src/pages/transactions_add/AddTransactionsPage.tsx
+++ src/pages/transactions_add/AddTransactionsPage.tsx
@@ -5,7 +5,7 @@ import "./AddTransactionsPage.css";
 import TransactionForm from "../../components/common/forms/transaction/TransactionForm";
 import TransactionTable from "../../components/transactions/transactions_table/TransactionsTable";
 import { Transaction } from "../../store/types/models";
-import IncomeForm from "../../components/common/forms/transaction/IncomeForm/IncomeForm";
+import IncomeForm from "../../components/common/forms/transaction/IncomeForm";
 import { TabPanel, tabProps } from "../../components/common/tabs";
 
 const ADD = 0;
