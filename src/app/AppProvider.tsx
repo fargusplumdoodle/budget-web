efe0cc6f26ade57488f35bf356d764a6e0e471d8
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-02-06 16:44:10 -0800

    refactor(core): Made Global Provider component

---
+++ src/app/AppProvider.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { ReactElement } from "react";
+import { LocalizationProvider } from "@mui/lab";
+import { ThemeProvider } from "@mui/material";
+import { darkTheme } from "./theme";
+import { SnackbarProvider } from "notistack";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../store/configureStore";
+
+type AppProvider = {
+  children: ReactElement[] | ReactElement;
+};
+
+const AppProvider: React.FC<AppProvider> = function ({ children }) {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <ThemeProvider theme={darkTheme}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <SnackbarProvider
+                  maxSnack={3}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                >
+                  {children}
+                </SnackbarProvider>
+              </LocalizationProvider>
+            </ThemeProvider>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+export default AppProvider;
