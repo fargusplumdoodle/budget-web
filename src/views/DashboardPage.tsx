bdf699690e78221b2d265401a42d34267caccd0f
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-04-10 14:37:45 -0700

    feat(elements): improved dashboard

--- src/views/DashboardPage.tsx
+++ src/views/DashboardPage.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import { FunctionComponent } from "react";
-import { Box, Grid } from "@mui/material";
+import { Box, Card, Grid, styled, Typography } from "@mui/material";
 import StatusOverview from "../components/dashboard/StatusOverview";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { RootState } from "../store/configureStore";
 import { Budget } from "../store/types/models";
 import { ProviderContext, withSnackbar } from "notistack";
+import LineGraph from "../components/report/LineGraph";
+import BudgetTable from "../components/budget/BudgetTable";
+import { ReportTypes } from "../api/types";
+import SpendingSummary from "../components/report/spending_summary/SpendingSummary";
+import { DateTime } from "luxon";
+import { EXPECTED_BUDGETS } from "../app/settings";
 
 interface OwnProps extends ProviderContext {
   budgets: Budget[];
@@ -13,10 +19,43 @@ interface OwnProps extends ProviderContext {
 
 type Props = OwnProps;
 
-const DashboardPage: FunctionComponent<Props> = ({ budgets }) => {
-  if (budgets.length === 0) {
+const CardArea = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  gap: theme.spacing(3),
+  height: "100%",
+  width: "100%",
+  opacity: 0.8,
+  flexWrap: "wrap",
+  marginTop: theme.spacing(3),
+}));
+
+const DashboardCard = styled(Card)(({ theme }) => ({
+  padding: theme.spacing(1),
+  maxWidth: `calc(50% - ${theme.spacing(4)})`,
+  minWidth: 300,
+  flexGrow: 1,
+  height: 520,
+  overflow: "scroll",
+  [theme.breakpoints.down(977)]: {
+    width: "100%",
+    maxWidth: "100%",
+  },
+}));
+
+const DashboardPage: FunctionComponent<Props> = () => {
+  const budgets = useSelector((state: RootState) => state.budgets);
+  if (budgets.list.length === 0) {
     return <></>;
   }
+  const savingsId = budgets.byName[EXPECTED_BUDGETS.SAVINGS]?.id || "";
+
+  const query = new URLSearchParams({
+    date__gte: DateTime.now().minus({ months: 3 }).toISODate(),
+    budgets__exclude: savingsId.toString(),
+  });
+
   return (
     <Box>
       <Box
@@ -27,13 +66,42 @@ const DashboardPage: FunctionComponent<Props> = ({ budgets }) => {
         <StatusOverview />
       </Box>
 
-      <Grid container flexDirection={["column", "row"]} spacing={1}></Grid>
+      <CardArea>
+        <DashboardCard>
+          <BudgetTable budgets={budgets.list} />
+        </DashboardCard>
+
+        <DashboardCard>
+          <Typography variant="h5">Balance</Typography>
+          <LineGraph
+            hideDateLte
+            hideTimebucketSelector
+            defaultTimebucketSize="one_week"
+            reportTypes={[ReportTypes.BUDGET_BALANCE]}
+          />
+        </DashboardCard>
+
+        <DashboardCard>
+          <Typography variant="h5">Pulse</Typography>
+          <LineGraph
+            queryParams={query}
+            hideDateLte
+            hideTimebucketSelector
+            defaultTimebucketSize="one_week"
+            reportTypes={[ReportTypes.INCOME, ReportTypes.OUTCOME]}
+          />
+        </DashboardCard>
+
+        <DashboardCard>
+          <Typography variant="h5">Spending Summary</Typography>
+          <SpendingSummary
+            queryParams={query}
+            defaultTimebucketSize="one_month"
+          />
+        </DashboardCard>
+      </CardArea>
     </Box>
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  budgets: state.budgets.list,
-});
-
-export default connect(mapStateToProps)(withSnackbar(DashboardPage));
+export default withSnackbar(DashboardPage);
