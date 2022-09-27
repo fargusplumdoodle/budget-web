5bb60f64a7b434a45ae00f8da8664713cb2e0208
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-09-26 19:41:30 -0700

    updated packages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,3 @@
-import { store } from "../src/store/configureStore";
-import { loadBudgetsSuccess } from "../src/store/actions/budgetActions";
-import { sampleBudgets, sampleTags } from "./store";
-import { loadTagsSuccess } from "../src/store/actions/tagActions";
-import AppProvider from "../src/app/AppProvider";
 import { withThemeDecorator } from "@fargusplumdoodle/themes";
 
 export const parameters = {
@@ -15,15 +10,4 @@ export const parameters = {
   },
 };
 
-const withProvider = (Story) => {
-  return (
-    <AppProvider>
-      <Story />
-    </AppProvider>
-  );
-};
-
-store.dispatch(loadBudgetsSuccess([...sampleBudgets]));
-store.dispatch(loadTagsSuccess([...sampleTags]));
-
-export const decorators = [withThemeDecorator, withProvider];
+export const decorators = [withThemeDecorator];
